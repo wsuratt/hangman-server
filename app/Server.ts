ff246963c9ad367982b58b36b3795f93b337968d
William Suratt <wsuratt@comcast.net>
2022-03-05 02:18:30 -0800

    test

--- app/Server.ts
+++ app/Server.ts
@@ -80,8 +80,6 @@ export async function updateGuess(userID: string, guess: string): Promise<string
   if (user.numGuesses > 0 && !user.hasWon && user.hasWagered)
   {
     let currTime = Date.now() / 1000;
-    console.log(currTime);
-    console.log(user.timestamp);
     if ((currTime - user.timestamp) > 15)
     {
       let str = String(user.targetWord);
